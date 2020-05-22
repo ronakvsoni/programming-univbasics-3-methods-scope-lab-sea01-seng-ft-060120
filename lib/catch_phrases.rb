7add63038e4b46b0709a848133b6047b4c05b435
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-22 18:00:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,10 +9,16 @@ def toadstool
    puts 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
 toadstool
+
+
 def link 
   puts "It's Dangerous To Go Alone! Take This."
 end
 link
+
+
+def all_phrases 
+  puts mario
 
 
 
